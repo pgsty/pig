505be96cbee3cc0c0ba05f75f09d441882f07857
Vonng <rh@vonng.com>
2025-02-14 19:23:09 +0800

    update default pig ext build pkg list

--- cli/ext/build.go
+++ cli/ext/build.go
@@ -8,27 +8,32 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// essential build tools for different linux distros
 var buildTools = map[string][]string{
-
 	"el8": {
-		"rpmdevtools", "createrepo_c", "wget", "dnf-utils", "dnf-plugins-core", "sshpass", "modulemd-tools", "ninja-build", "openssl-devel", "pkg-config", "make", "cmake",
-		"pgdg-srpm-macros", "postgresql1*-devel", "postgresql1*-server", "readline-devel", "zlib-devel", "lz4", "lz4-devel", "libzstd-devel", "openssl-devel",
+		"make", "cmake", "ninja-build", "pkg-config", "git", "lz4", "unzip", "ncdu", "rsync",
+		"rpmdevtools", "dnf-utils", "pgdg-srpm-macros", "postgresql1*-devel", "postgresql1*-server",
+		"readline-devel", "zlib-devel", "libxml2-devel", "lz4-devel", "libzstd-devel", "krb5-devel",
 	},
 	"el9": {
-		"rpmdevtools", "createrepo_c", "wget", "dnf-utils", "dnf-plugins-core", "sshpass", "modulemd-tools", "ninja-build", "openssl-devel", "pkg-config", "make", "cmake",
-		"pgdg-srpm-macros", "postgresql1*-devel", "postgresql1*-server", "readline-devel", "zlib-devel", "lz4", "lz4-devel", "libzstd-devel", "openssl-devel",
+		"make", "cmake", "ninja-build", "pkg-config", "git", "lz4", "unzip", "ncdu", "rsync",
+		"rpmdevtools", "dnf-utils", "pgdg-srpm-macros", "postgresql1*-devel", "postgresql1*-server",
+		"readline-devel", "zlib-devel", "libxml2-devel", "lz4-devel", "libzstd-devel", "krb5-devel",
 	},
 	"d12": {
-		"lz4", "unzip", "wget", "patch", "bash", "lsof", "sshpass", "debhelper", "devscripts", "fakeroot", "pkg-config", "make", "cmake", "ncdu", "rsync",
-		"postgresql-all", "postgresql-server-dev-all", "libreadline-dev", "flex", "bison", "libxml2-dev", "libxml2-utils", "xsltproc", "libc++-dev", "libc++abi-dev", "libglib2.0-dev", "libtinfo5", "libstdc++-12-dev", "liblz4-dev", "ninja-build",
+		"make", "cmake", "ninja-build", "pkg-config", "git", "lz4", "unzip", "ncdu", "rsync",
+		"debhelper", "devscripts", "fakeroot", "postgresql-all", "postgresql-server-dev-all",
+		"libreadline-dev", "zlib1g-dev", "libxml2-dev", "liblz·4-dev", "libzstd-dev", "libkrb5-dev",
 	},
 	"u22": {
-		"lz4", "unzip", "wget", "patch", "bash", "lsof", "sshpass", "debhelper", "devscripts", "fakeroot", "pkg-config", "make", "cmake", "ncdu", "rsync",
-		"postgresql-all", "postgresql-server-dev-all", "libreadline-dev", "flex", "bison", "libxml2-dev", "libxml2-utils", "xsltproc", "libc++-dev", "libc++abi-dev", "libglib2.0-dev", "libtinfo6", "libstdc++-12-dev", "liblz4-dev", "ninja-build",
+		"make", "cmake", "ninja-build", "pkg-config", "git", "lz4", "unzip", "ncdu", "rsync",
+		"debhelper", "devscripts", "fakeroot", "postgresql-all", "postgresql-server-dev-all",
+		"libreadline-dev", "zlib1g-dev", "libxml2-dev", "liblz4-dev", "libzstd-dev", "libkrb5-dev",
 	},
 	"u24": {
-		"lz4", "unzip", "wget", "patch", "bash", "lsof", "sshpass", "debhelper", "devscripts", "fakeroot", "pkg-config", "make", "cmake", "ncdu", "rsync",
-		"postgresql-all", "postgresql-server-dev-all", "libreadline-dev", "flex", "bison", "libxml2-dev", "libxml2-utils", "xsltproc", "libc++-dev", "libc++abi-dev", "libglib2.0-dev", "libtinfo6", "libstdc++-12-dev", "liblz4-dev", "ninja-build",
+		"make", "cmake", "ninja-build", "pkg-config", "git", "lz4", "unzip", "ncdu", "rsync",
+		"debhelper", "devscripts", "fakeroot", "postgresql-all", "postgresql-server-dev-all",
+		"libreadline-dev", "zlib1g-dev", "libxml2-dev", "liblz4-dev", "libzstd-dev", "libkrb5-dev",
 	},
 }
 
