c6bfef0dd1e3086f53b32175f0dae96a59e93910
Feng Ruohang <rh@vonng.com>
2025-07-16 18:33:17 +0800

    change the summary link

--- cli/ext/extension.go
+++ cli/ext/extension.go
@@ -49,7 +49,7 @@ type Extension struct {
 
 // SummaryURL returns the URL to the pigsty.io catalog summary page
 func (e *Extension) SummaryURL() string {
-	return fmt.Sprintf("%s/ext/%s", config.PgstyCom, e.Name)
+	return fmt.Sprintf("https://ext.pgsty.com/e/%s", e.Name)
 }
 
 // CompactVersion returns the compact version string like 17-13
